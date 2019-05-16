5595d170e3fc2370442ba8b51f6a80d8edab56e4
Oldac <48088461+Oldac@users.noreply.github.com>
2019-05-16 23:50:14 +0300

    m

--- SE311LiveStockProject/src/Visitor.java
+++ SE311LiveStockProject/src/Visitor.java
@@ -2,14 +2,14 @@ import java.time.LocalDate;
 //"Visitor"
 abstract public class Visitor {
 	protected static int id=1; 
-	protected LocalDate dateOf=LocalDate.now();
+	protected LocalDate dateOf=LocalDate.now(); 
 	public abstract void Visit(DairyCattle dairyCattle);
 	public abstract void Visit(BeefCattle BeefCattle);
 }
 //"ConcreteVisitor1 - veterinaryVisitor"
 class veterinaryVisitor extends Visitor{
 	public void Visit(DairyCattle dairyCattle) {
-		dairyCattle.setId(id);
+		dairyCattle.setId(id); 
 		dairyCattle.setvaccinationInfo(dairyCattle.getName() +" has been injected :"+dateOf+"(Cattle id :"+dairyCattle.getId()+")");
 		System.out.println(dairyCattle.getvaccinationInfo());
 		id++;
